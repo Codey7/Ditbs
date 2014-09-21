af8f57cf1bfd2e16b5fe5b749106e117b436dacf
bsm888 <1102768969@qq.com>
2014-09-21 23:01:57 +0800

    Signed-off-by: bsm888 <1102768969@qq.com>

---
+++ src/com/tes/tview/MainActivity.java
@@ -0,0 +1,76 @@
+package com.tes.tview;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class MainActivity extends ActionBarActivity {
+
+	private ListView lv = null; 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		SimpleAdapter adapter = new SimpleAdapter(this,getData(),R.layout.vlist,
+				new String[]{"title","info","img"},
+				new int[]{R.id.title,R.id.info,R.id.img});
+		lv=(ListView)findViewById(R.id.ListView01);
+		lv.setAdapter(adapter);     
+		
+	}
+
+	private List<Map<String, Object>> getData() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("title", "G1");
+		map.put("info", "google 1");
+		map.put("img", R.drawable.sb);
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "G2");
+		map.put("info", "google 2");
+		map.put("img", R.drawable.sb);
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "G3");
+		map.put("info", "google 3");
+		map.put("img", R.drawable.sb);
+		list.add(map);
+		
+		return list;
+	}
+
+	
+	
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
